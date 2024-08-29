a05a1fcdd5cb26bb833dcb22c5a141cafc02689b
hemantjoon <hemant.joon@rcb.res.in>
2024-08-29 03:39:34 +0200

    survival plot initial

--- server.R
+++ server.R
@@ -26,89 +26,54 @@ function(input, output, session) {
     }
   )
   
-  # Reactive to update the selectInput choices based on selectedDataMode
-  observeEvent(input$selectedDataMode, {
+  # Reactive to determine the selected dataset
+  dataset <- reactive({
     if (input$selectedDataMode == "ex1") {
-      updateSelectInput(session, "selectedGene", 
-                        choices = c("sex", "TP53", "EGFR"),
-                        selected = "sex")  # Set default selected value
-      
-      output$dataTable <- DT::renderDataTable({
-        DT::datatable(
-          data = exampleData1,
-          options = list(
-            pageLength = 20,
-            autoWidth = FALSE
-          )
-        )
-      })
-      
+      updateSelectInput(session, "selectedGene", choices = c("sex", "TP53", "EGFR"), selected = "sex")
+      return(exampleData1)
     } else if (input$selectedDataMode == "ex2") {
-      updateSelectInput(session, "selectedGene", 
-                        choices = c("PIK3CA", "TP63", "BRCA1"),
-                        selected = "PIK3CA")  # Set default selected value
-      output$dataTable <- DT::renderDataTable({
-        DT::datatable(
-          data = exampleData2,
-          options = list(
-            pageLength = 20,
-            autoWidth = FALSE
-          )
-        )
-      })
+      updateSelectInput(session, "selectedGene", choices = c("PIK3CA", "TP63", "BRCA1"), selected = "PIK3CA")
+      return(exampleData2)
     } else if (input$selectedDataMode == "ex3") {
-      
-      output$dataTable <- DT::renderDataTable({
-        
-        # render only if there is data available
-        req(input_file())
-        
-        # reactives are only callable inside an reactive context like render
-        data <- input_file()
-        
-        updateSelectInput(session, "selectedGene", 
-                          choices = colnames(data),
-                          selected = "filter1")
-        
-        
-        #data <- subset(data, dateCreated >= input$period[1] & dateCreated <= input$period[2])
-        DT::datatable(
-          data = data,
-          options = list(
-            pageLength = 20,
-            autoWidth = FALSE
-          )
-        )
-      })
-      
-      
-      
+      req(input_file())
+      updateSelectInput(session, "selectedGene", choices = colnames(input_file()), selected = "filter1")
+      return(input_file())
     }
-    
-    
-    
+  })
+  
+  # Rendering the dataTable
+  output$dataTable <- DT::renderDataTable({
+    DT::datatable(
+      data = dataset(),
+      options = list(pageLength = 20, autoWidth = FALSE)
+    )
   })
   
   input_file <- reactive({
     if (is.null(input$selectedFile)) {
-      return("")
+      return(NULL)
     }
-    
+
     # actually read the file
     data <- read.csv(file = input$selectedFile$datapath, sep = input$selectedSeparator)
     colnames(data) <- gsub("\\.", "_", colnames(data))
     data
   })
   
-  
-  fit <- survfit(Surv(time, status) ~ sex,
-                 data = exampleData1)
-  
-  output$survivalPlot <- renderPlot(
-    #Visualize with survminer
+
+  fit <- reactive({
     
-    ggsurvplot(fit, data = exampleData1, risk.table = FALSE)
-  )
+    duration <<- dataset()[[input$selectedDurationColumn]]
+    outcome <<- dataset()[[input$selectedOutcomeColumn]]
+    gene <<- dataset()[[input$selectedGene]]
+    
+    survfit(Surv(duration, outcome) ~ gene, data = dataset())
+  })
+  
+  # Render the survival plot
+  output$survivalPlot <- renderPlot({
+    ggsurvplot(fit(), data = dataset())
+  })
   
   
 }
